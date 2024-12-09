760117267503af967af4cd3197651547476924b1
Denis Mraovic <denis.mraovic@gmail.com>
2024-12-09 21:51:55 +0100

    Init
    Update
    
    Update
    
    Video for monster loops
    
    Better grid
    
    Fix for carousel
    
    Stop youtube video when carousel closes
    
    Added more projects
    
    Freelancer
    
    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+# dmraovic.github.io
+
+Personal GitHub page serving as a portfolio showcasing projects which were
+developed recently. For each project a screenshot or logo is provided, as well
+as a brief description, used technology stack and links to associated Git
+repositories when available.
+
+## Getting started
+
+To view this page in your browser, you can:
+* visit the instance hosted at
+[dmraovic.github.io](https://skomaromi.github.io/); or
+* clone this repository and view the local copy from your computer.
---
+++ res/js/main.js
@@ -0,0 +1,32 @@
+window.onload = main;
+
+function main() {
+    // initialize Masonry
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer'
+    });
+
+    // set current copyright year
+    var timeNow = new Date();
+    var yearSpan = $('.page-footer #year');
+    yearSpan.text(timeNow.getFullYear());
+    
+    // randomize silly letters
+    const letters = document.querySelectorAll('#alive-text span');
+
+    letters.forEach(letter => {
+        const randomDelay = Math.random() * 2;
+        letter.style.animationDelay = `${randomDelay}s`;
+    });
+
+    // stop youtube video playback when the modal closes
+    $(document).on('hidden.bs.modal', function (event) {
+        $(event.target).find('iframe').each(function () {
+            const iframe = $(this);
+            const src = iframe.attr('src');
+            iframe.attr('src', '');
+            iframe.attr('src', src);
+        });
+    });
+}
